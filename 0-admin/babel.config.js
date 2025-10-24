96b7e60801277a9a9a673f3cfbe434cb40702953
labubu <labubu@example.com>
2025-10-24 14:28:52 +0700

    修改打包编译插件

--- 0-admin/babel.config.js
+++ 0-admin/babel.config.js
@@ -1,13 +1,11 @@
 module.exports = {
   presets: [
-    // https://github.com/vuejs/vue-cli/tree/master/packages/@vue/babel-preset-app
-    '@vue/cli-plugin-babel/preset'
+    ['@babel/preset-env', { useBuiltIns: 'usage', corejs: 3 }],
+    '@vue/babel-preset-jsx'
   ],
-  'env': {
-    'development': {
-      // babel-plugin-dynamic-import-node plugin only does one thing by converting all import() to require().
-      // This plugin can significantly increase the speed of hot updates, when you have a large number of pages.
-      'plugins': ['dynamic-import-node']
+  env: {
+    development: {
+      plugins: ['dynamic-import-node']
     }
   }
 }
